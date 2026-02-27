f18787b317bc434a97d8f7506f559da16bf64b78
huku30 <apranilprasai@gmail.com>
2026-02-27 22:41:10 +0545

    updated link on Aayu Malun Khola Hydropower

--- frontend/src/app/Notices/AayuMalunKholaHydropower/page.tsx
+++ frontend/src/app/Notices/AayuMalunKholaHydropower/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Link from "next/link";
 
-export default function AayuMalunKholaHydropowerPage() {
+export default function AayuMalunKholahydropowerPage() {
   return (
     <>
       <Navbar />
